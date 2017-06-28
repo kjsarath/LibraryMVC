848e1e05af15fd5b300af0f15154ff4fca68994f
kjsarath <jayachandran.sarath@gmail.com>
2017-06-28 17:18:36 +0400

    commit msg-graph 2

--- LibraryMVC/Controllers/MainController.cs
+++ LibraryMVC/Controllers/MainController.cs
@@ -1,4 +1,5 @@
 ﻿using LibraryMVC.DAL;
+using LibraryMVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -11,6 +12,8 @@ namespace LibraryMVC.Controllers
 {
     public class MainController : Controller
     {
+        OrisonSystemSCHOOLEntities db = new OrisonSystemSCHOOLEntities();
+
         [HttpGet]
         [OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
         public ActionResult Index()
@@ -29,25 +32,49 @@ namespace LibraryMVC.Controllers
         public JsonResult Chart1()
         {
             List<object> iData = new List<object>();
-            List<string> labels = new List<string>();
-            //labels.Add("January");
-            //labels.Add("February");
-            //labels.Add("March");
-            //labels.Add("April");
-            List<int> lst_dataItem_1 = new List<int>();
-            //lst_dataItem_1.Add(1);
-            //lst_dataItem_1.Add(2);
-            //lst_dataItem_1.Add(3);
-            //lst_dataItem_1.Add(4);
-            //iData.Add(labels);
-            //iData.Add(lst_dataItem_1);
-            OrisonSystemSCHOOLEntities db = new OrisonSystemSCHOOLEntities();
-            DataTable dtAcc = clsDBOperations.GetTable("Select * from dbo.Library_IssueReturnComparisonFn", db);
+            List<object> ilbl = new List<object>();
+            DataTable dtAcc = clsDBOperations.GetTable("Select * from dbo.Library_IssueReturnComparisonFn()", db);
+            foreach (DataColumn dc in dtAcc.Columns)
+            {
+                List<object> x = new List<object>();
+                x = (from DataRow dr in dtAcc.Rows select dr[dc.ColumnName]).ToList();
+                iData.Add(x);
+            }
+            return Json(iData, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Chart2()
+        {
+            //List<InOutComparisonData> iData = new List<InOutComparisonData>();
+            //List<object> ilbl = new List<object>();
+            //string[] arrColor = new string[] { "#231F20", "#FFC200", "#F44937", "#16F27E", "#FC9775", "#5A69A6" };
+            //DataTable dtAcc = clsDBOperations.GetTable("Select * from dbo.[Library_InOutComparisonFn]()", db);
+            //foreach (DataColumn dc in dtAcc.Columns)
+            //{
+            //    InOutComparisonData x = new InOutComparisonData();
+            //    x.label=dc.ColumnName ;
+            //    x.value = dtAcc.Rows[0][dc.ColumnName];
+            //    x.color = arrColor[dtAcc.Columns.IndexOf(dc)];
+            //    iData.Add(x);
+            //}
+            List<object > iData = new List<object>();
+            List<object> ilbl = new List<object>();
+            DataTable dtAcc = clsDBOperations.GetTable("Select * from dbo.[Library_InOutComparisonFn]()", db);
+            foreach (DataColumn dc in dtAcc.Columns)
+            {
+                List<object> x = new List<object>();
+                x = (from DataRow dr in dtAcc.Rows select dr[dc.ColumnName]).ToList();
+                iData.Add(x);
+            }   
             return Json(iData, JsonRequestBehavior.AllowGet);
         }
     }
-
-    internal class OrisonSystemSCHOOLEntities
-    {
-    }
+    
+}
+public class InOutComparisonData
+{
+    public string label { get; set; }
+    public object value { get; set; }
+    public string color { get; set; }
 }
